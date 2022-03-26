287c9b529a435b19abd7400e04eaf9ebfd4c0ef4
GabrielMoretti <gabrielmorettisilva@gmail.com>
2022-03-26 19:26:39 -0300

    feature: CreateAdmin

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,22 @@
+import createConnection from '@shared/infra/typeorm'
+import { hash } from 'bcrypt';
+import {v4 as uuidv4} from 'uuid'
+
+async function create(){
+  const connection = await createConnection();
+
+  const id = uuidv4();
+
+  const password = hash('gabrieldev', 8)
+
+  await connection.query(`INSERT INTO customers
+  (id, name, email, password, avatar, admin, created_at, updated_at)
+  VALUES('${id}', 'adminAmatronic', 'gabrieldev@gmail.com', '${password}',
+  null, true, 'now()', 'now()')`)
+
+  await connection.close();
+}
+
+create().then(()=>{
+  console.log('User admin created sucessufully!')
+})
