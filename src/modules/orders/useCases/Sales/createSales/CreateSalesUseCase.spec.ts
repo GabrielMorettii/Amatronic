a822ecf6ec3fa40769a02566a8e6556ab5c82656
GabrielMoretti <gabrielmorettisilva@gmail.com>
2022-04-01 16:17:35 -0300

    feature: CreateSalesUseCaseTest - 2

--- src/modules/orders/useCases/Sales/createSales/CreateSalesUseCase.spec.ts
+++ src/modules/orders/useCases/Sales/createSales/CreateSalesUseCase.spec.ts
@@ -35,4 +35,32 @@ describe('Create Sales Use Case', ()=>{
     expect(sale.totalValue).toEqual(10251.15);
     expect(good.amount).toBe(4);
   })
+
+  it('Should not be able to create a new sale with invalid good', async ()=>{
+    await expect(
+      createSalesUseCase.execute(
+        '1231',
+        5
+      )
+    ).rejects.toEqual(new AppError('The good was not found!', 404))
+  })
+
+  it('Should not be able to create a new sale with good out of stock', async ()=>{
+    const good = await fakeGoodsRepository.create({
+      name: "Iphone X",
+      amount: 9,
+      price: 2050.23,
+      description: "Iphone da 10 geração",
+      category_id: '123',
+      brand_id:  '123'
+    })
+
+
+    await expect(
+      createSalesUseCase.execute(
+        good.id,
+        10
+      )
+    ).rejects.toEqual(new AppError('Out of stock!'))
+  })
 })
